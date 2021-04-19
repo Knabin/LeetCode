7b80bd22298ef45bb74e4577162b8702ee2a1b74
Nabin Kim <knabinn97@gmail.com>
2021-04-19 12:58:36 +0900

    Update 1460. Make Two Arrays Equal by Reversing Sub-arrays

--- 1401_1500/1460_make-two-arrays-equal-by-reversing-sub-arrays.cpp
+++ 1401_1500/1460_make-two-arrays-equal-by-reversing-sub-arrays.cpp
@@ -25,3 +25,48 @@ public:
         return true;
     }
 };
+
+
+/*
+ * 04/19/2021 12:51
+ * Status   Accepted	
+ * Runtime  16 ms
+ * Memory   13.9 MB
+ */
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        int check[1001] { 0 };
+        for (int i = 0; i < target.size(); ++i)
+        {
+            ++check[target[i]];
+            --check[arr[i]];
+        }
+        for (int i = 0; i <= 1000; ++i)
+        {
+            if (check[i] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+/*
+ * 04/19/2021 12:54
+ * Status   Accepted	
+ * Runtime  16 ms
+ * Memory   14 MB
+ */
+
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        sort(begin(target), end(target));
+        sort(begin(arr), end(arr));
+        
+        return target == arr;
+    }
+};
